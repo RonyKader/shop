ac05a1828a3c558ff2818adcde3a508dcab5df2a
RonyKader <tarikul.islam@chefonline.co.uk>
2016-10-09 15:01:17 +0100

    dbconfig

---
+++ commonClass/Database.php
@@ -0,0 +1,71 @@
+<?php 
+	class Database
+	{
+		public $host 	= DB_HOST;
+		public $user 	= DB_USER;
+		public $pass    = DB_PASS;
+		public $db_name = DB_NAME;
+
+		public $link;
+		public $error;
+
+		public function __construct()
+		{
+			$this->connectionDB();
+		}
+
+		private function connectionDB()
+		{
+			$this->link = new mysqli( $this->host, $this->user, $this->pass, $this->db_name );
+			if ( !$this->link ) 
+			{
+				$this->error = "Connection Fail". $this->link->connect_error;
+				return false;
+			}
+		}
+
+
+		public function select($query)
+		{
+			$result = $this->link->query($query) or die( $this->link->error.__LINE__);
+			if ( $result->num_rows > 0 ) 
+			{
+				return $result;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public function insert($query)
+		{
+			$result = $this->link->query($query) or die( $this->link->error.__LINE__);
+			if ( $result ) 
+			{
+				return $result;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public function update($query)
+		{
+			$result = $this->link->query($query) or die( $this->link->error.__LINE__);
+			if ( $result ) 
+			{
+				return $result;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+
+	}
+
+ ?>
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php 
+	define("DB_HOST", "localhost");
+	define("DB_USER", "w3zoncm_tarik");
+	define("DB_PASS", "RonyKader9666");
+	define("DB_NAME", "w3zoncm_elspress");
+
+ ?>
